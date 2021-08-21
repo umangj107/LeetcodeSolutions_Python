c2ba0dfc152cf8313f5ff75bed72c3e45e63fad6
umangj107 <61326269+umangj107@users.noreply.github.com>
2021-08-21 12:10:22 +0530

    Create MaximumSubarray.py
    Leetcode Question 53. Maximum Subarray

---
+++ Arrays/MaximumSubarray.py
@@ -0,0 +1,11 @@
+#Using Kadane's Algorithm
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum = nums[0]
+        max_sum = nums[0]
+        
+        for i in range(1, len(nums)):
+            current_sum = max(current_sum + nums[i], nums[i])
+            max_sum = max(max_sum, current_sum)
+            
+        return max_sum

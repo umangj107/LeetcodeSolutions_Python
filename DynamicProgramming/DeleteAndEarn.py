7a708392a4dbf49370a5663e970ec3ebef0a4e31
Umang Jain <umangj107@gmail.com>
2022-03-05 21:03:46 +0530

    Updated Solution to Leetcode Question 740. Delete and Earn

--- DynamicProgramming/DeleteAndEarn.py
+++ DynamicProgramming/DeleteAndEarn.py
@@ -1,13 +1,28 @@
+# Approach-1
+
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        num = [0]*(max(nums)+1)
-        for i in nums:
-            num[i] += i
-        dp = [0]*len(num)
-        if len(num) == 1:
-            return num[0]
-        dp[0] = num[0]
-        dp[1] = max(num[0], num[1])
-        for i in range(2, len(num)):
-            dp[i] = max(dp[i-2]+num[i], dp[i-1])
-        return dp[-1]
+        refDict = Counter(nums)
+        n = max(nums) + 1
+        dp = [0 for _ in range(n)]
+        dp[0] = refDict.get(0, 0)
+        dp[1] = max(refDict.get(0, 0), refDict.get(1, 0))
+
+        for i in range(2, n):
+            dp[i] = max(i * refDict.get(i, 0) + dp[i-2], dp[i-1])
+
+        return dp[n-1]
+
+
+# Approach-2
+
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        refDict = Counter(nums)
+        prev = refDict.get(0, 0)
+        curr = refDict.get(1, 0)
+
+        for i in range(2, max(refDict.keys())+1):
+            prev, curr = curr, max(prev + i * refDict.get(i, 0), curr)
+
+        return curr

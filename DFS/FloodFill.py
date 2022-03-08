cc5ce45a14fbcbfea38057ad366010c6757204c7
Umang Jain <umangj107@gmail.com>
2022-03-08 12:10:42 +0530

    Updated Solution to leetcode Question 733. Flood Fill

--- DFS/FloodFill.py
+++ DFS/FloodFill.py
@@ -1,14 +1,21 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        def dfs(image, r, c, col, newCol):
-            if r >= len(image) or r < 0 or c >= len(image[0]) or c < 0 or image[r][c] != col:
-                return
-            else:
-                image[r][c] = newCol
-                for x, y in [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]:
-                    if 0 <= x < len(image) and 0 <= y < len(image[0]) and image[x][y] == col:
-                        dfs(image, x, y, col, newCol)
+        startColor = image[sr][sc]
+        rows = len(image)
+        cols = len(image[0])
+        queue = [(sr, sc)]
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
 
-        if image[sr][sc] != newColor:
-            dfs(image, sr, sc, image[sr][sc], newColor)
+        while queue:
+            temp = []
+            while queue:
+                x, y = queue.pop(0)
+                image[x][y] = newColor
+                visited[x][y] = True
+                for r, c in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]:
+                    if 0 <= r < rows and 0 <= c < cols and not visited[r][c] and image[r][c] == startColor:
+                        temp.append((r, c))
+
+            queue = temp
+
         return image

2c5ebc75cefcb20109c367bfb1b6bb965e181c7c
Umang Jain <umangj107@gmail.com>
2022-03-10 23:36:20 +0530

    Updated solution to Leetcode Question 35. Search Insert Position

--- SearchingAndSorting/SearchInsertPosition.py
+++ SearchingAndSorting/SearchInsertPosition.py
@@ -1,15 +1,15 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        end = len(nums) - 1
         start = 0
-        if start == end:
-            return 0 if target <= nums[0] else 1
+        end = len(nums) - 1
+
         while start <= end:
             mid = start + ((end - start) // 2)
             if nums[mid] == target:
                 return mid
-            elif target < nums[mid]:
-                end = mid - 1
-            else:
+            elif nums[mid] < target:
                 start = mid + 1
-        return mid if target <= nums[mid] else mid + 1
+            else:
+                end = mid - 1
+
+        return start

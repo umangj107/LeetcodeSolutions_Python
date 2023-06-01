e1303b0e03c2f3716c63c60c9d300206eaded185
Umang Jain <umangj107@gmail.com>
2023-06-01 12:35:40 +0530

    Better solution for 1091. Shortest Path in Binary Matrix

--- BFS/ShortestPathInBinaryMatrix.py
+++ BFS/ShortestPathInBinaryMatrix.py
@@ -1,31 +1,25 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-
-        if rows * cols == 1:
-            return 1 if grid[0][0] == 0 else -1
-
-        if grid[0][0] == 1 or (grid[1][0] == 1 and grid[0][1] == 1 and grid[1][1] == 1) or grid[rows-1][cols-1] == 1:
+        n = len(grid)
+        if grid[0][0] == 1:
             return -1
 
-        curr_moves = 1
-
         queue = [(0, 0)]
-        grid[0][0] = 1
-        val = grid[0][0]
+        moves = 1
 
         while queue:
-            temp = []
+            tempQueue = []
             while queue:
                 x, y = queue.pop(0)
-                if x == rows-1 and y == cols-1:
-                    return curr_moves
-                for r, c in [(x+1, y), (x, y+1), (x-1, y), (x, y-1), (x+1, y+1), (x-1, y-1), (x+1, y-1), (x-1, y+1)]:
-                    if 0 <= r < rows and 0 <= c < cols and grid[r][c] == 0:
-                        temp.append((r, c))
-                        grid[r][c] = 1
-            curr_moves += 1
-            queue = temp
+                if x == n-1 and y == n-1:
+                    return moves
+                for i, j in [(x, y+1), (x, y-1), (x+1, y), (x-1, y),
+                             (x+1, y-1), (x-1, y+1), (x+1, y+1), (x-1, y-1)]:
+                    if 0 <= i < n and 0 <= j < n and grid[i][j] == 0:
+                        tempQueue.append((i, j))
+                        grid[i][j] = 1
+
+            moves += 1
+            queue = tempQueue
 
         return -1
